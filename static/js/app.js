8fa3b701e33548a68524f166bf3e400008b3e97a
Nicholas <krylik@rigaming.co.za>
2015-02-20 11:45:58 +0200

    Adding fontawesome, Fixing graph no-values error

--- static/js/app.js
+++ static/js/app.js
@@ -16,13 +16,15 @@ $('document').ready(function(){
     $('#entitySearchForm').on('submit', function(event) {
         // Prevent the page reload.
         event.preventDefault();
+        // Switch search icon to working icon.
+        $('#entitySearchForm button i').removeClass('fa-search').addClass('fa-gears');
         // Get the type of entity we're looking for to pass through to zkill.
         // The eve entity search seems to treat these all the same.
         var entityType = $('#entityType').val();
         var entityName = $('#entityName').val();
         entitySearch(entityName, function(err, entity) {
             if (!err) {
-                if (entity.characterID !== 0) {
+                if (entity.characterID != 0) {
                     /*
                         Setting the no-items and no-attackers results in a much
                         smaller payload returned from zkill.
@@ -34,7 +36,7 @@ $('document').ready(function(){
                         than 200 kills in a day, so you're not going to get a
                         proper representation of activity over a period.
                         I would probably need to implement some sort of range,
-                        doing requests for all the requests in a day, for each
+                        doing requests for all the kills in a day, for each
                         of the last 7 days. But I don't want to do too many
                         requests to zkill, so this will have to do.
                     */
@@ -45,15 +47,20 @@ $('document').ready(function(){
                     };
                     zkilloptions[entityType + 'ID'] = entity.characterID;
                     zkillSearch(zkilloptions, function(err, kills) {
+                        // Switch working icon to search icon.
+                        $('#entitySearchForm button i').removeClass('fa-gears').addClass('fa-search');
                         // Here we're going to normalize the kill time into hours
                         // We need to accumulate before we can normalize for
                         // highcharts.
                         var killTimes = {};
+                        // We have to seed this with all the hours, otherwise
+                        // highcharts just makes shit up for the hours that arent
+                        // in the data.
+                        for (var i = 0; i < 24; i++) {
+                            killTimes[i] = 0;
+                        }
                         kills.forEach(function(kill) {
                             var hour = moment(kill.killTime).hour();
-                            if (!killTimes[hour]) {
-                                killTimes[hour] = 0;
-                            }
                             killTimes[hour] += 1;
                         });
                         // The one thing i've found about highcharts, is that its
@@ -72,7 +79,12 @@ $('document').ready(function(){
                                 text: 'Kills and Deaths per hour'
                             },
                             xAxis: {
-                                allowDecimals: false
+                                allowDecimals: false,
+                                labels: {
+                                    formatter: function () {
+                                        return (this.value < 10 ? '0' + this.value : this.value) + ':00';
+                                    }
+                                }
                             },
                             yAxis: {
                                 title: {
@@ -92,9 +104,11 @@ $('document').ready(function(){
                     });
                 } else {
                     console.log("No entity with that name found in Eve");
+                    $('#entitySearchForm button i').removeClass('fa-gears').addClass('fa-search');
                 }
             } else {
                 console.log(err);
+                $('#entitySearchForm button i').removeClass('fa-gears').addClass('fa-search');
             }
         });
     });
